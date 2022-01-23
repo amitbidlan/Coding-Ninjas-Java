38cac7c9cfbd792830261887eb84d04161485336
amitbidlan <amitbidlan@localhost>
2022-01-23 13:51:46 +0530

    Sixth Commit

---
+++ Arrays-1/src/IntroductionToArrays.java
@@ -0,0 +1,11 @@
+
+public class IntroductionToArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = new int[10];
+		arr[0] = 10;
+		System.out.println(arr[0]);
+	}
+
+}
