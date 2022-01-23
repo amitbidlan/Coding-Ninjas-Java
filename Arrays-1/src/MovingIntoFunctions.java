7c6cd59693a2bd1faa9706971228091e9fe60249
amitbidlan <amitbidlan@localhost>
2022-01-23 15:04:57 +0530

    Involving functions in taking and printing input

---
+++ Arrays-1/src/MovingIntoFunctions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class MovingIntoFunctions {
+	
+	public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0;i<n;i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+		}
+	
+	public static void printArr(int[] arr) {
+		int n = arr.length;
+		for(int i = 0;i<n;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = takeInput();
+		printArr(arr);
+	}
+
+}
